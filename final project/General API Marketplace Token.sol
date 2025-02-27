06ca1f65d281c2a27c228651a4232237005613c8
broxthemighty <broxthemighty@gmail.com>
2025-02-27 11:19:15 -0600

    changed pragma back.

--- final project/General API Marketplace Token.sol
+++ final project/General API Marketplace Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -12,7 +12,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
  * @dev General API Marketplace Contract
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
- 
+
 contract ApiMarketplace is ReentrancyGuard, AccessControl {
     using SafeERC20 for IERC20;
 
